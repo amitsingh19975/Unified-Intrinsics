683ad098c16535c3ead351b8ae5a59e52ab9076a
Amit Singh <amitsingh19975@gmail.com>
2025-02-26 00:26:47 +0530

    start with left shift test

---
+++ test/runtime/shift_test.cpp
@@ -0,0 +1,147 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include <concepts>
+#include <functional>
+#include <limits>
+#include <numeric>
+#include <print>
+#include "ui.hpp"
+#include "ui/arch/arm/shift.hpp"
+#include <cmath>
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+
+using namespace ui;
+
+template <std::size_t N, typename T>
+struct DataGenerator {
+	static constexpr auto make() noexcept -> Vec<N, T> {
+		std::array<T, N> data;
+		std::iota(data.begin(), data.end(), 1);
+		data[0] = std::numeric_limits<T>::min();
+		data[1] = std::numeric_limits<T>::max();
+		return Vec<N, T>::load(data.data(), data.size());
+	}
+
+	static constexpr auto cyclic_make(T min, T max) noexcept -> Vec<N, T> {
+		std::array<T, N> data;
+		for (auto i = 0u; i < N; ++i) {
+			data[i] = static_cast<T>(min + (N % (max + 1)));
+		}
+		return Vec<N, T>::load(data.data(), data.size());
+	}
+};
+
+template <unsigned I>
+using index_t = std::integral_constant<unsigned, I>;
+
+template <std::size_t... Is, typename Fn>
+	requires (std::invocable<Fn, index_t<0>>)
+auto for_each(Fn&& fn) {
+	(std::invoke(fn, index_t<Is>{}), ...);
+}
+
+TEST_CASE( VEC_ARCH_NAME " Left Shift", "[shift][left]" ) {
+    GIVEN("Signed 8bit Integer") {
+		using type = std::int8_t;
+		using utype = std::make_unsigned_t<type>;
+		static constexpr auto N = 32ul;
+		auto v = DataGenerator<N, type>::make();
+		INFO("[Vec]: " << std::format("{}", v));
+		static constexpr auto min = std::numeric_limits<type>::min();
+		static constexpr auto max = std::numeric_limits<type>::max();
+		THEN("Elements are correct") {
+			REQUIRE(v[0] == min);
+			REQUIRE(v[1] == max);
+			for (auto i = 3u; i < N; ++i) REQUIRE(v[i - 1] == i);
+		}
+
+		WHEN("Normal left shift using compile-time count") {
+			for_each<1, 2, 3, 4, 5, 6, 7>([&]<unsigned I>(index_t<I>){
+				auto res = shift_left<I>(v);
+				for (auto i = 0u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << " << I << ")[" << int(type(v[i] << I)) <<"] == " << int(res[i]));
+					REQUIRE(static_cast<type>(v[0] << I) == res[0]);
+				}
+			});
+		}
+
+		WHEN("Normal left shift using runtime count") {
+			auto s = DataGenerator<N, std::make_unsigned_t<type>>::cyclic_make(1, 7);
+			auto res = shift_left(v, s);
+			for (auto i = 0u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << " << s[i] << ")[" << int(type(v[i] << s[i])) <<"] == " << int(res[i]));
+				REQUIRE(static_cast<type>(v[i] << s[i]) == res[i]);
+			}
+		}
+
+		WHEN("Saturating left shift compile-time count") {
+			auto res = sat_shift_left<2>(v); // v << 2
+			REQUIRE(res[0] == min);
+			REQUIRE(res[1] == max);
+			for (auto i = 2u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << 2)[" << int(type(v[i] << 2)) <<"] == " << int(res[i]));
+				REQUIRE(static_cast<type>(std::min(int(v[i]) << 2, int(max))) == res[i]);
+			}
+		}
+
+		WHEN("Saturating left shift runtime count") {
+			auto res = sat_shift_left(v, Vec<N, utype>::load(2)); // sat(v << 2)
+			REQUIRE(res[0] == min);
+			REQUIRE(res[1] == max);
+			for (auto i = 2u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << 2)[" << int(type(v[i] << 2)) <<"] == " << int(res[i]));
+				REQUIRE(static_cast<type>(std::min(int(v[i]) << 2, int(max))) == res[i]);
+			}
+		}
+
+		WHEN("Rounding left shift") {
+			auto res = rounding_shift_left(v, Vec<N, utype>::load(2));
+			for (auto i = 0u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << " << 2 << ")[" << int(type(v[i] << 2)) <<"] == " << int(res[i]));
+				REQUIRE(static_cast<type>(v[i] << 2) == res[i]);
+			}
+		}
+
+		WHEN("Saturting rounding left shift") {
+			auto res = sat_rounding_shift_left(v, Vec<N, utype>::load(2));
+			REQUIRE(res[0] == min);
+			REQUIRE(res[1] == max);
+			for (auto i = 2u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << 2)[" << int(type(v[i] << 2)) <<"] == " << int(res[i]));
+				REQUIRE(static_cast<type>(std::min(int(v[i]) << 2, int(max))) == res[i]);
+			}
+		}
+
+		WHEN("Widening left shift") {
+			auto res = widening_shift_left<2>(v);
+			static_assert(std::same_as<decltype(res)::element_t, std::int16_t>);
+			for (auto i = 0u; i < N; ++i) {
+				INFO("['" << i << "']: (" << int(v[i]) << " << 2)[" << int(type(v[i] << 2)) <<"] == " << int(res[i]));
+				REQUIRE((v[i] << 2) == res[i]);
+			}
+		}
+
+		WHEN("Insert left shift") {
+			auto bs = sat_add(v, Vec<N, type>::load(33));
+			auto res = insert_shift_left<2>(v, bs);
+			std::println("{:0b}\n{:0b}", v, res);
+			constexpr auto mask = utype((1 << 2) - 1);
+			for (auto i = 0u; i < N; ++i) {
+				auto a = type(v[i]) & mask;
+				auto b = type(bs[i]); 
+				auto sb = type((b << 2) & ~mask);
+
+				auto ans = a | sb;
+
+				INFO("['" << i << "']: " <<
+					std::format("{} == {}", ans, res[i])
+				);
+				REQUIRE(ans == res[i]);
+			}
+		}
+    }
+}
+
