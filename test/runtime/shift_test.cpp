05a5096e15d6856297c99b81d2cff2bd420d0453
Amit Singh <amitsingh19975@gmail.com>
2025-02-28 19:48:26 +0530

    refactor code

--- test/runtime/shift_test.cpp
+++ test/runtime/shift_test.cpp
@@ -5,53 +5,13 @@
 #include <functional>
 #include <algorithm>
 #include <limits>
-#include <numeric>
 #include <print>
 #include "ui.hpp"
 #include <cstdint>
 #include <type_traits>
-#include <random>
-#include <vector>
+#include "utils.hpp"
 
 using namespace ui;
-
-template <std::size_t N, typename T>
-struct DataGenerator {
-    static constexpr auto make() noexcept -> Vec<N, T> {
-        std::array<T, N> data;
-        data[0] = std::numeric_limits<T>::min();
-        data[1] = std::numeric_limits<T>::max();
-        if constexpr (N > 2) std::iota(data.begin() + 2, data.end(), 0);
-        return Vec<N, T>::load(data.data(), data.size());
-    }
-
-    static constexpr auto cyclic_make(T min, T max) noexcept -> Vec<N, T> {
-        std::array<T, N> data;
-        for (auto i = 0u; i < N; ++i) {
-            data[i] = static_cast<T>(min + (N % (max + 1)));
-        }
-        return Vec<N, T>::load(data.data(), data.size());
-    }
-
-    static constexpr auto random(std::size_t seed = 0) noexcept -> Vec<N, T> {
-        std::mt19937 rng(seed);
-        std::array<T, N> data;
-        if constexpr (std::integral<T>) {
-            std::uniform_int_distribution<T> dist(
-               std::numeric_limits<T>::min(),
-               std::numeric_limits<T>::max()
-            );
-            for (auto i = 0ul; i < N; ++i) data[i] = dist(rng);
-        } else {
-            std::uniform_int_distribution<T> dist(-100, 100);
-            for (auto i = 0ul; i < N; ++i) data[i] = dist(rng);
-        }
-        return Vec<N, T>::load(data.data(), data.size());
-    }
-};
-
-template <unsigned I>
-using index_t = std::integral_constant<unsigned, I>;
 
 template <std::size_t... Is, typename Fn>
     requires (std::invocable<Fn, index_t<2>>)
---
+++ test/runtime/utils.hpp
@@ -0,0 +1,45 @@
+#include <concepts>
+#include <limits>
+#include <numeric>
+#include <print>
+#include "ui.hpp"
+#include <random>
+
+template <unsigned I>
+using index_t = std::integral_constant<unsigned, I>;
+
+template <std::size_t N, typename T>
+struct DataGenerator {
+    static constexpr auto make() noexcept -> ui::Vec<N, T> {
+        std::array<T, N> data;
+        data[0] = std::numeric_limits<T>::min();
+        data[1] = std::numeric_limits<T>::max();
+        if constexpr (N > 2) std::iota(data.begin() + 2, data.end(), 0);
+        return ui::Vec<N, T>::load(data.data(), data.size());
+    }
+
+    static constexpr auto cyclic_make(T min, T max) noexcept -> ui::Vec<N, T> {
+        std::array<T, N> data;
+        for (auto i = 0u; i < N; ++i) {
+            data[i] = static_cast<T>(min + (N % (max + 1)));
+        }
+        return ui::Vec<N, T>::load(data.data(), data.size());
+    }
+
+    static constexpr auto random(std::size_t seed = 0) noexcept -> ui::Vec<N, T> {
+        std::mt19937 rng(seed);
+        std::array<T, N> data;
+        if constexpr (std::integral<T>) {
+            std::uniform_int_distribution<T> dist(
+               std::numeric_limits<T>::min(),
+               std::numeric_limits<T>::max()
+            );
+            for (auto i = 0ul; i < N; ++i) data[i] = dist(rng);
+        } else {
+            std::uniform_int_distribution<T> dist(-100, 100);
+            for (auto i = 0ul; i < N; ++i) data[i] = dist(rng);
+        }
+        return ui::Vec<N, T>::load(data.data(), data.size());
+    }
+};
+
