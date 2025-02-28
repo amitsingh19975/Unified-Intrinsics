87853d6d0f1e6fc1e712901a33954062c0209c58
Amit Singh <amitsingh19975@gmail.com>
2025-02-28 17:22:40 +0530

    add more tests, and fix tests

--- test/runtime/shift_test.cpp
+++ test/runtime/shift_test.cpp
@@ -991,24 +991,182 @@ TEST_CASE( VEC_ARCH_NAME " Right Shift", "[shift][right]" ) {
 
         WHEN("Insert right shift") {
             auto bs = sat_add(v, Vec<N, type>::load(33));
-            std::println("BS: {}", bs);
             auto res = insert_shift_right<2>(v, bs);
             INFO(std::format("insert(v, bs): {:x}", res));
-            REQUIRE(res[0] == -0x7c);
-            REQUIRE(res[1] == -0x1);
-            REQUIRE(res[2] == -0x7c);
-            REQUIRE(res[3] == -0x77);
-            REQUIRE(res[4] == -0x72);
-            REQUIRE(res[5] == -0x6d);
-            REQUIRE(res[6] == -0x6c);
-            REQUIRE(res[7] == -0x67);
-            REQUIRE(res[8] == -0x62);
-            REQUIRE(res[9] == -0x5d);
-            REQUIRE(res[10] == -0x5c);
-            REQUIRE(res[11] == -0x57);
-            REQUIRE(res[12] == -0x52);
-            REQUIRE(res[13] == -0x4d);
-            REQUIRE(res[31] == -0x7);
+            REQUIRE(res[0] == -0x58);
+            REQUIRE(res[1] ==  0x5f);
+            REQUIRE(res[2] ==  0x08);
+            REQUIRE(res[3] ==  0x08);
+            REQUIRE(res[4] ==  0x08);
+            REQUIRE(res[5] ==  0x09);
+            REQUIRE(res[6] ==  0x09);
+            REQUIRE(res[7] ==  0x09);
+            REQUIRE(res[8] ==  0x09);
+            REQUIRE(res[9] ==  0x0a);
+            REQUIRE(res[10] ==  0x0a);
+            REQUIRE(res[11] ==  0x0a);
+            REQUIRE(res[12] ==  0x0a);
+            REQUIRE(res[13] ==  0x0b);
+            REQUIRE(res[31] ==  0x0f);
+        }
+    }
+
+    GIVEN("Unsigned 8bit Integer") {
+        using type = std::uint8_t;
+        using utype = std::make_unsigned_t<type>;
+        static constexpr auto N = 32ul;
+        auto v = DataGenerator<N, type>::make();
+        INFO("[Vec]: " << std::format("{}", v));
+        static constexpr auto min = std::numeric_limits<type>::min();
+        static constexpr auto max = std::numeric_limits<type>::max();
+        THEN("Elements are correct") {
+            REQUIRE(v[0] == min);
+            REQUIRE(v[1] == max);
+            for (auto i = 2u; i < N; ++i) REQUIRE(v[i] == i - 2);
+        }
+
+        WHEN("Normal right shift using compile-time count") {
+            for_each<1, 2, 3, 4, 5, 6, 7>([&]<unsigned I>(index_t<I>){
+                auto res = shift_right<I>(v);
+                for (auto i = 0u; i < N; ++i) {
+                INFO(std::format("[{}]: ({:08b} >> {})[{:08b}] == {:08b}", i, v[i], I, type(v[i] >> I), res[i]));
+                    REQUIRE(static_cast<type>(v[i] >> I) == res[i]);
+                }
+            });
+        }
+
+        WHEN("Normal right shift using runtime count") {
+            auto s = DataGenerator<N, std::make_unsigned_t<type>>::cyclic_make(1, 7);
+            auto res = shift_right(v, s);
+            for (auto i = 0u; i < N; ++i) {
+                INFO(std::format("[{}]: ({:08b} >> {})[{:08b}] == {:08b}", i, v[i], s[i], type(v[i] >> s[i]), res[i]));
+                REQUIRE(static_cast<type>(v[i] >> s[i]) == res[i]);
+            }
+        }
+
+        WHEN("Saturating left shift runtime count") {
+            auto res = sat_shift_right(v, Vec<N, utype>::load(2)); // sat(v >> 2)
+            REQUIRE(res[0] == min >> 2);
+            REQUIRE(res[1] == max >> 2);
+            for (auto i = 2u; i < N; ++i) {
+                INFO(std::format("[{}]: ({:08b} >> {})[{:08b}] == {:08b}", i, v[i], 2, type(v[i] >> 2), res[i]));
+                REQUIRE(static_cast<type>(std::clamp(type(v[i] >> 2), min, max)) == res[i]);
+            }
+        }
+
+        WHEN("Rounding right shift") {
+            auto res = rounding_shift_right(v, Vec<N, utype>::load(2));
+            using wtype = ui::internal::widening_result_t<type>;
+            for (auto i = 0u; i < N; ++i) {
+                auto temp = wtype(v[i] + (wtype(1) << (2 - 1))); 
+                INFO(std::format("[{}]: ({:08b} >> {})[{:08b}] == {:08b}", i, temp, 2, type(temp >> 2), res[i]));
+                REQUIRE(static_cast<type>(temp >> 2) == res[i]);
+            }
+        }
+
+        WHEN("Saturting rounding right shift") {
+            auto res = sat_rounding_shift_right(v, Vec<N, utype>::load(2));
+            INFO(std::format("[sat_round(V >> 2)]: {}", res));
+            REQUIRE(res[0] == 0);
+            REQUIRE(res[1] == 64);
+            REQUIRE(res[2] == 0);
+            REQUIRE(res[3] == 0);
+            REQUIRE(res[4] == 1);
+            REQUIRE(res[5] == 1);
+            REQUIRE(res[6] == 1);
+            REQUIRE(res[7] == 1);
+            REQUIRE(res[8] == 2);
+            REQUIRE(res[9] == 2);
+            REQUIRE(res[10] == 2);
+            REQUIRE(res[11] == 2);
+            REQUIRE(res[12] == 3);
+            REQUIRE(res[13] == 3);
+            REQUIRE(res[14] == 3);
+            REQUIRE(res[15] == 3);
+            REQUIRE(res[16] == 4);
+            REQUIRE(res[17] == 4);
+            REQUIRE(res[18] == 4);
+            REQUIRE(res[19] == 4);
+            REQUIRE(res[20] == 5);
+            REQUIRE(res[31] == 7);
+        }
+
+        WHEN("Accumulating rounding right shift") {
+            auto res = rounding_shift_right_accumulate<2>(Vec<N, type>::load(1), v);
+            INFO(std::format("Vec({}s) + ({} >> 2) == {:0x}", 1, v, res));
+            REQUIRE(res[0] == 1);
+            REQUIRE(res[1] == 65);
+            REQUIRE(res[2] == 1);
+            REQUIRE(res[3] == 1);
+            REQUIRE(res[4] == 2);
+            REQUIRE(res[5] == 2);
+            REQUIRE(res[6] == 2);
+            REQUIRE(res[7] == 2);
+            REQUIRE(res[8] == 3);
+            REQUIRE(res[9] == 3);
+            REQUIRE(res[10] == 3);
+            REQUIRE(res[11] == 3);
+            REQUIRE(res[12] == 4);
+            REQUIRE(res[13] == 4);
+            REQUIRE(res[14] == 4);
+            REQUIRE(res[15] == 4);
+            REQUIRE(res[16] == 5);
+            REQUIRE(res[17] == 5);
+            REQUIRE(res[18] == 5);
+            REQUIRE(res[19] == 5);
+            REQUIRE(res[20] == 6);
+            REQUIRE(res[31] == 8);
+        }
+
+        WHEN("Narrowing right shift") {
+            /*auto res = narrowing_shift_right<2>(v);*/
+            // This not compile
+        }
+
+        WHEN("Saturating narrowing right shift") {
+            /*auto res = sat_narrowing_shift_right<2>(v);*/
+            // This not compile
+        }
+
+        WHEN("Rounding narrowing right shift") {
+            /*auto res = rounding_narrowing_shift_right<2>(v);*/
+            // This not compile
+        }
+
+        WHEN("Saturating unsigned narrowing right shift") {
+            /*auto res = sat_unsigned_narrowing_shift_right<2>(v);*/
+            // This not compile
+        }
+
+        WHEN("Saturating rounding narrowing right shift") {
+            /*auto res = sat_rounding_narrowing_shift_right<2>(v);*/
+            // This not compile
+        }
+
+        WHEN("Saturating rounding unsigned narrowing right shift") {
+            /*auto res = sat_rounding_unsigned_narrowing_shift_right<2>(v);*/
+            // This not compile
+        }
+
+        WHEN("Insert right shift") {
+            auto bs = sat_add(v, Vec<N, type>::load(33));
+            auto res = insert_shift_right<2>(v, bs);
+            INFO(std::format("insert(v, bs): {:x}", res));
+            REQUIRE(res[0]  ==  0x08);
+            REQUIRE(res[1]  ==  0xff);
+            REQUIRE(res[2]  ==  0x08);
+            REQUIRE(res[3]  ==  0x08);
+            REQUIRE(res[4]  ==  0x08);
+            REQUIRE(res[5]  ==  0x09);
+            REQUIRE(res[6]  ==  0x09);
+            REQUIRE(res[7]  ==  0x09);
+            REQUIRE(res[8]  ==  0x09);
+            REQUIRE(res[9]  ==  0x0a);
+            REQUIRE(res[10] ==  0x0a);
+            REQUIRE(res[11] ==  0x0a);
+            REQUIRE(res[12] ==  0x0a);
+            REQUIRE(res[13] ==  0x0b);
+            REQUIRE(res[31] ==  0x0f);
         }
     }
 }
