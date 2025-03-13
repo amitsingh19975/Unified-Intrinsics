c0b100423f5e453038295be54c4750ebb3ee16a0
amitsingh19975 <amitsingh19975@gmail.com>
2025-03-13 09:18:06 +0000

    fix bugs and refactor code

--- include/ui/vec_headers.hpp
+++ include/ui/vec_headers.hpp
@@ -5,20 +5,20 @@
 
 #ifdef UI_EMPSCRIPTEN_WASM_SIMD
     #include <wasm_simd128.h>
-    #define UI_NATIVE_SIZE sizeof(v128_t)
+    #define UI_NATIVE_SIZE 16
 #elif UI_CPU_SSE_LEVEL >= UI_CPU_SSE_LEVEL_AVX
     #include <immintrin.h>
     #if UI_CPU_SSE_LEVEL >= UI_CPU_SSE_LEVEL_SKX
-        #define UI_NATIVE_SIZE sizeof(__m512)
+        #define UI_NATIVE_SIZE 64
     #else
-        #define UI_NATIVE_SIZE sizeof(__m256)
+        #define UI_NATIVE_SIZE 32
     #endif
 #elif UI_CPU_SSE_LEVEL >= UI_CPU_SSE_LEVEL_SSE41
     #include <smmintrin.h>
-    #define UI_NATIVE_SIZE sizeof(__m128)
+    #define UI_NATIVE_SIZE 16
 #elif UI_CPU_SSE_LEVEL >= UI_CPU_SSE_LEVEL_SSE1
     #include <xmmintrin.h>
-    #define UI_NATIVE_SIZE sizeof(__m128)
+    #define UI_NATIVE_SIZE 16
 #elif defined(UI_ARM_HAS_NEON)
     #include <arm_neon.h>
     #if defined __has_include
